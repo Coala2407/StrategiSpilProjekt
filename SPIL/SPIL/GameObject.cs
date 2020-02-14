cf067b21bcf5fc7daca397d5c6a1040ce6e11bbd
Nic-Zedler <niclas-zedler@hotmail.com>
2020-02-14 12:14:53 +0100

    Committing nothing

--- SPIL/SPIL/GameObject.cs
+++ SPIL/SPIL/GameObject.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SPIL
+﻿namespace SPIL
 {
     public abstract class GameObject
     {
-
     }
 }
