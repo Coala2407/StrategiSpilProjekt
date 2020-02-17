b649e3f967876b3722f2878370039c465d9e214b
Coala2407 <36776480+Coala2407@users.noreply.github.com>
2020-02-17 10:03:42 +0100

    Update Tile.cs

--- SPIL/SPIL/Tile.cs
+++ SPIL/SPIL/Tile.cs
@@ -10,8 +10,63 @@ namespace SPIL
 {
     class Tile : GameObject
     {
+        public enum Mode
+        {
+            StartTile = 0,
+            EndTile = 1
+        }
+        public enum Direction
+        {
+            TurnUp = 0,
+            TurnRight = 1,
+            TurnDown = 2,
+            TurnLeft = 3
+        }
 
+        /// <summary>
+        /// Makes the units turn on the map. Not used for all tiles.
+        /// </summary>
+        Direction tileDirection;
+        public Direction TileDirection { get; set; }
+
+        /// <summary>
+        /// Only used for the first and last tile. Sets unit spawn/despawn location.
+        /// </summary>
+        Mode tileMode;
+        public Mode TileMode { get; set; }
+
+        /// <summary>
+        /// Default tile constructor
+        /// </summary>
         public Tile()
+        {
+            initialize();
+        }
+
+        /// <summary>
+        /// Change the turn direction of the tile. Makes the units turn
+        /// </summary>
+        /// <param name="tiledirection"></param>
+        public Tile(Direction tiledirection)
+        {
+            TileDirection = tiledirection;
+            initialize();
+        }
+
+        /// <summary>
+        /// Only used for first and last tile. Sets unit spawn/despawn locations.
+        /// </summary>
+        /// <param name="tileMode"></param>
+        public Tile(Mode tileMode)
+        {
+            TileMode = tileMode;
+            initialize();
+        }
+
+        /// <summary>
+        /// Runs on all constructors
+        /// </summary>
+        private void initialize()
         {
             size = 0.25f;
             sprite = Assets.GrassTile1.FirstOrDefault();
