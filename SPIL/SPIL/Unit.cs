bc9535dca2f7b0b89ebc6282ddb5a61621532a92
Nic-Zedler <niclas-zedler@hotmail.com>
2020-02-17 11:04:30 +0100

    Unit stuff

--- SPIL/SPIL/Unit.cs
+++ SPIL/SPIL/Unit.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace SPIL
 {
-    class Unit
+    class Unit : GameObject
     {
+        int health = 4;
+        
+        bool alive = true;
+
+        float speed = 0.5f;
+
+        int goldTaken;
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public bool IsDead
+        {
+            get { return health <= 0; }
+        }
+
+        public int GoldTaken
+        {
+            get { return goldTaken; }
+        }
+
+        public override void OnCollision(GameObject otherObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if(health <= 0)
+            {
+                alive = false;
+            }
+
+        }
     }
 }
